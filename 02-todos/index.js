97ed8319ba05939c2a9bfb2cc3d2f6c30c8f4985
Tran Xuan Hoang <hoangtx.ict@gmail.com>
2020-07-09 22:38:28 +0900

    Define a menu for the app and config the quit menu

--- 02-todos/index.js
+++ 02-todos/index.js
@@ -1,15 +1,41 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
+  // Open up a main window when the app started up
   mainWindow = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true
     }
   });
-
   mainWindow.loadFile('main.html');
+
+  // Setup and show an app menu
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+// Define the app menu template with label and submenu items
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      { label: 'New Todo' },
+      {
+        label: 'Quit',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+// Tweak the menu on MacOS so that all menu items will be shown as expected
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({});
+}
