b232c4d5c569b824d35f2ccbd883f73891d1e6e9
Tran Xuan Hoang <hoangtx.ict@gmail.com>
2020-07-11 22:40:59 +0900

    Refactor the source code using ES6 object oriented approach

---
+++ 03-tasky/app/timer_tray.js
@@ -0,0 +1,38 @@
+const electron = require('electron');
+const { Tray } = electron;
+
+class TimerTray extends Tray {
+  constructor(iconPath, mainWindow) {
+    super(iconPath);
+    this.mainWindow = mainWindow;
+    this.on('click', this.onClick.bind(this));
+  }
+
+  // Toggle the mainWindow when user clicks the tray icon
+  onClick(event, bounds) {
+    if (this.mainWindow.isVisible()) {
+      this.mainWindow.hide();
+    } else {
+      // Mouse click event location
+      const { x, y } = bounds;
+
+      // Window heigh and width
+      const { height, width } = this.mainWindow.getBounds();
+
+      // Calculate the y position based on the OS on which the app will run
+      const yPosition = process.platform === 'darwin' ? y : y - height;
+
+      // Position the window right before showing it
+      this.mainWindow.setBounds({
+        x: x - width / 2,
+        y: yPosition,
+        width,
+        height
+      });
+
+      this.mainWindow.show();
+    }
+  }
+}
+
+module.exports = TimerTray;
--- 03-tasky/main.js
+++ 03-tasky/main.js
@@ -1,10 +1,10 @@
 const electron = require('electron');
 const path = require('path');
+const TimerTray = require('./app/timer_tray');
 
-const { app, BrowserWindow, Tray } = electron;
+const { app, BrowserWindow } = electron;
 
 let mainWindow;
-let tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -23,31 +23,5 @@ app.on('ready', () => {
   // Define the tray
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-  tray = new Tray(iconPath);
-
-  // Toggle the mainWindow when user clicks the tray icon
-  tray.on('click', (event, bounds) => {
-    if (mainWindow.isVisible()) {
-      mainWindow.hide();
-    } else {
-      // Mouse click event location
-      const { x, y } = bounds;
-
-      // Window heigh and width
-      const { height, width } = mainWindow.getBounds();
-
-      // Calculate the y position based on the OS on which the app will run
-      const yPosition = process.platform === 'darwin' ? y : y - height;
-
-      // Position the window right before showing it
-      mainWindow.setBounds({
-        x: x - width / 2,
-        y: yPosition,
-        width,
-        height
-      });
-
-      mainWindow.show();
-    }
-  });
+  new TimerTray(iconPath, mainWindow);
 });
