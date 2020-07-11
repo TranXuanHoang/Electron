a12871d3f9af5e826a97cdbfae03f75c9c421a8b
Tran Xuan Hoang <hoangtx.ict@gmail.com>
2020-07-11 15:46:53 +0900

    Define the app tray icon and its logic to toggle showing the app window
    * Set to not show the app window frame
    * Define the app tray icon that will be displayed in the top right corner in MacOS and in the bottom right one in Windows
    * Add logic to show or hide the app window when user clicks the tray icon

--- 03-tasky/main.js
+++ 03-tasky/main.js
@@ -1,15 +1,36 @@
 const electron = require('electron');
+const path = require('path');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    height: 500,
+    width: 300,
+    frame: false,
+    resizable: false,
+    show: false
   });
 
   mainWindow.loadFile('./src/index.html');
+
+  // Define the tray
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+  tray = new Tray(iconPath);
+
+  // Toggle the mainWindow when user clicks the tray icon
+  tray.on('click', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  });
 });
