82dac73d5692bbe238ff1810a34a342dc2eb1465
Tran Xuan Hoang <hoangtx.ict@gmail.com>
2020-07-12 21:39:08 +0900

    Pick video files and retrieve their duration
    * Pick a list of videos using web file picker (not the Electron)
    * Use FFMPEG to get the metadata of these videos
    * Pass the list of videos and their metadata between the main process and the Electron app process using IPC

--- 04-video-converter/main.js
+++ 04-video-converter/main.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
 
@@ -15,4 +16,28 @@ app.on('ready', () => {
   });
 
   mainWindow.loadFile('./src/index.html');
+});
+
+ipcMain.on('videos:added', (event, videos) => {
+  // Wrap each video metadata retrieving logic in a promise
+  // and put these promises in an array
+  const promises = videos.map((video) => {
+    return new Promise((resolve, reject) => {
+      // Get video file metadata
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        resolve({
+          ...video,
+          duration: metadata.format.duration,
+          format: 'avi'
+        });
+      });
+    });
+  });
+
+  // Use Promise.all to wait for all promises finish (resolve) its logic
+  // before returning metadata of all videos back to the React side
+  Promise.all(promises)
+    .then(results => {
+      mainWindow.send('metadata:complete', results);
+    });
 });
