080b27e959b08c5e213bd5a10783569798953cde
Tran Xuan Hoang <hoangtx.ict@gmail.com>
2020-08-04 01:38:28 +0900

    Select items with mouse click and/or arrow keys
    * Indicate item selected by mouse clicking or arrow up/down key pushing with different left border line color
    * Define function to handle double clicks on each item

--- 06-bookmark/renderer/items.js
+++ 06-bookmark/renderer/items.js
@@ -8,6 +8,43 @@ exports.save = () => {
   localStorage.setItem('readit-items', JSON.stringify(this.storage))
 }
 
+// Indicate item as selected
+exports.select = e => {
+  // Unselect currently selected item
+  document.getElementsByClassName('read-item selected')[0].classList.remove('selected')
+
+  // Indicate newly selected item with different color on its left border
+  e.currentTarget.classList.add('selected')
+}
+
+// Move to newly selected item with arrow up/down keys
+exports.changeSelection = direction => {
+  // Get currently selected item
+  let currentItem = document.getElementsByClassName('read-item selected')[0]
+
+  // Handle up/down key
+  if (direction == 'ArrowUp' && currentItem.previousSibling) {
+    currentItem.classList.remove('selected')
+    currentItem.previousSibling.classList.add('selected')
+  } else if (direction == 'ArrowDown' && currentItem.nextSibling) {
+    currentItem.classList.remove('selected')
+    currentItem.nextSibling.classList.add('selected')
+  }
+}
+
+// Open selected item
+exports.open = () => {
+  // Only proceed to open an item if there exists at lest one
+  if (!this.storage.length) return
+
+  // Get item currently being selected
+  let selectedItem = document.getElementsByClassName('read-item selected')[0]
+
+  // Get item's URL
+  let contentURL = selectedItem.dataset.url
+  console.log('Opening item: ', contentURL)
+}
+
 // Add new item
 exports.addItem = (item, isNew = false) => {
   // Create a new DOM node
@@ -16,11 +53,25 @@ exports.addItem = (item, isNew = false) => {
   // Assign "read-item" class
   itemNode.setAttribute('class', 'read-item')
 
+  // Set item url as data attribute
+  itemNode.setAttribute('data-url', item.url)
+
   // Add inner HTML
   itemNode.innerHTML = `<img src="${item.screenshot}"><h3>${item.title}</h3>`
 
   // Append new DOM node to the list of "items"
   items.appendChild(itemNode)
+
+  // Attach click handler to each item
+  itemNode.addEventListener('click', this.select)
+
+  // Attach double click handler to each item
+  itemNode.addEventListener('dblclick', this.open)
+
+  // If this is the first item, select it
+  if (document.getElementsByClassName('read-item').length === 1) {
+    itemNode.classList.add('selected')
+  }
 
   // Also append the item to the "storage" array and persist it
   if (isNew) {
